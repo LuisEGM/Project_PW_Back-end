e06f36fd00811bfe91cf5f87620dfe6e6112ae98
LuisEGM <gamezluis777@gmail.com>
2021-12-03 01:42:41 -0500

    feat(*): Agregando el modelo del producto

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,9 @@
 from sqlalchemy import Column, Integer, String
 from app.database.session import Base
 
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Integer)
+    image = Column(String)
